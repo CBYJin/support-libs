5ae371904fc2014a9bcbd4986d579dbaecc4673a
CBYJin <cabyjin@gmail.com>
2025-06-16 22:43:39 -0700

    added hitool files

---
+++ HiTool/Resources/Common/JTAG/target/Hi3716DV110/Hi3716DV110.tcl
@@ -0,0 +1,12 @@
+# Chip definition for HiSilicon 3716 (using ARM tap)
+#
+source [find bitsbytes.tcl]
+source [find cpu/arm/cortex_a9.tcl]
+source [find memory.tcl]
+source [find mmr_helpers.tcl]
+
+set CHIP_MAKER	hisilicon
+set CHIP_FAMILY hs3716
+set CHIPNAME    hs3716
+
+
